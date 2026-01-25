95b781dfcd161550120062bff3c50593b3db3153
Aaron Joyce <2ajoyce@gmail.com>
2026-01-24 22:32:31 -0600

    feat: implement GetMe and GetActiveShard endpoints
    - Added endpoint constants for /accounts/me and /active-shards
    - Added ActiveShard model
    - Implemented GetMe method to get account by access token
    - Implemented GetActiveShard method to get active shard for a player
    - Added tests for new methods"

--- riot/account/account.go
+++ riot/account/account.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/KnutZuidema/golio/api"
+	"github.com/KnutZuidema/golio/internal"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -40,6 +41,41 @@ func (ac *Client) GetByRiotID(gameName, tagLine string) (*Account, error) {
 		return nil, err
 	}
 	return &account, nil
+}
+
+// GetMe returns the account for the given access token
+func (ac *Client) GetMe(accessToken string) (*Account, error) {
+	logger := ac.logger().WithField("method", "GetMe")
+	var account Account
+	c := *ac.c
+	c.Region = api.Region(api.RegionToRoute[c.Region])
+
+	if err := c.GetInto(
+		endpointGetMe,
+		&account,
+		internal.WithHeader("Authorization", "Bearer "+accessToken),
+	); err != nil {
+		logger.Debug(err)
+		return nil, err
+	}
+	return &account, nil
+}
+
+// GetActiveShard returns the active shard for a player
+func (ac *Client) GetActiveShard(game, puuid string) (*ActiveShard, error) {
+	logger := ac.logger().WithField("method", "GetActiveShard")
+	var activeShard ActiveShard
+	c := *ac.c
+	c.Region = api.Region(api.RegionToRoute[c.Region])
+
+	if err := c.GetInto(
+		fmt.Sprintf(endpointActiveShards, game, puuid),
+		&activeShard,
+	); err != nil {
+		logger.Debug(err)
+		return nil, err
+	}
+	return &activeShard, nil
 }
 
 func (ac *Client) logger() log.FieldLogger {
--- riot/account/model.go
+++ riot/account/model.go
@@ -6,3 +6,10 @@ type Account struct {
 	GameName string `json:"gameName"`
 	TagLine  string `json:"tagLine"`
 }
+
+// ActiveShard contains information about the active shard for a player
+type ActiveShard struct {
+	Puuid       string `json:"puuid"`
+	Game        string `json:"game"`
+	ActiveShard string `json:"activeShard"`
+}
